ef73e689f06ad80eedacb8983dc6946875a69af8
Cristopher Grandjean <crgrandj@c1r8s3.42lausanne.ch>
2026-08-17 18:41:46 +0200

    all

--- C02/ex08/ft_strlowcase.c
+++ C02/ex08/ft_strlowcase.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: crgrandj <crgrandj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/08/17 17:21:50 by crgrandj          #+#    #+#             */
-/*   Updated: 2026/08/17 17:48:19 by crgrandj         ###   ########.fr       */
+/*   Created: 2026/08/17 17:49:27 by crgrandj          #+#    #+#             */
+/*   Updated: 2026/08/17 17:52:40 by crgrandj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strupcase(char *str)
+char	*ft_strlowcase(char *str)
 {
 	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (str[i] >= 'A' && str[i] <= 'Z')
 		{
-			str[i] = str[i] - 32;
+			str[i] = str[i] + 32;
 			i++;
 		}
 		else
